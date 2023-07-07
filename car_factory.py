3dae1ec74411e783359d459eff0be1d3863f1465
Allison H <44848576+yhuang6012@users.noreply.github.com>
2023-07-08 01:07:35 +1000

    Add files via upload

--- car_factory.py
+++ car_factory.py
@@ -9,26 +9,26 @@ from nubbin_battery import NubbinBattery
 class CarFactory():
     def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
         if (CapuletEngine.needs_service(current_mileage, last_service_mileage) == True 
-            and SpindlerBattery.needs_service(last_service_date, current_date) == True):
+            or SpindlerBattery.needs_service(last_service_date, current_date) == True):
             Car(CapuletEngine(current_mileage, last_service_mileage), SpindlerBattery(last_service_date, current_date))
     
     
     def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
         if (WilloughbyEngine.needs_service(current_mileage, last_service_mileage) == True 
-            and SpindlerBattery.needs_service(last_service_date, current_date) == True):
+            or SpindlerBattery.needs_service(last_service_date, current_date) == True):
             Car(WilloughbyEngine(current_mileage, last_service_mileage), SpindlerBattery(last_service_date, current_date))
     
     
     def create_palindrome(current_date, last_service_date, warning_light_on):
-        if SternmanEngine.needs_service(warning_light_on) == True and NubbinBattery.needs_service(last_service_date, current_date) == True:
+        if SternmanEngine.needs_service(warning_light_on) == True or NubbinBattery.needs_service(last_service_date, current_date) == True:
             Car(SternmanEngine(warning_light_on), NubbinBattery(last_service_date, current_date))
     
     
     def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
-        if WilloughbyEngine.needs_service(current_mileage, last_service_mileage) == True and NubbinBattery.needs_service(last_service_date, current_date) == True:
+        if WilloughbyEngine.needs_service(current_mileage, last_service_mileage) == True or NubbinBattery.needs_service(last_service_date, current_date) == True:
             Car(WilloughbyEngine(current_mileage, last_service_mileage), NubbinBattery(last_service_date, current_date))
         
         
     def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
-        if CapuletEngine.needs_service(current_mileage, last_service_mileage) == True and  NubbinBattery.needs_service(last_service_date, current_date) == True:
+        if CapuletEngine.needs_service(current_mileage, last_service_mileage) == True or  NubbinBattery.needs_service(last_service_date, current_date) == True:
             Car(CapuletEngine(current_mileage, last_service_mileage), NubbinBattery(last_service_date, current_date))
